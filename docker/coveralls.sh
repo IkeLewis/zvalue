2db0d205779d179947de6f7205736a68bb401fd2
Isaac Lewis <isaac.b.lewis@gmail.com>
2017-07-26 18:26:06 -0400

    Pass the environment variable 'COVERALLS_REPO_TOKEN' from Travis CI to docker.

--- docker/coveralls.sh
+++ docker/coveralls.sh
@@ -8,8 +8,7 @@ if [ "${UPLOAD_COVERAGE}" == "yes" ]; then
 
     cpp-coveralls --verbose \
 		  --encodings utf-8 latin-1 \
-		  -E ".*/googletest.*" -E ".*/CMake.*" \
-		  -t $COVERALLS_REPO_TOKEN
+		  -E ".*/googletest.*" -E ".*/CMake.*"
 
 elif [ -z "${UPLOAD_COVERAGE}" ]; then
 
